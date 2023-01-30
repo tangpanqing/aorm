2aedde0193083f9da3191951de063e25ba15f657
tangpanqing <tangpanqing@qq.com>
2023-01-30 11:43:02 +0800

    update GetOne

--- builder/exists.go
+++ builder/exists.go
@@ -2,14 +2,13 @@ package builder
 
 // Exists 存在某记录
 func (b *Builder) Exists() (bool, error) {
-	var obj IntStruct
-
-	err := b.selectCommon("", "1 AS c", nil, "").Limit(0, 1).GetOne(&obj)
+	rows, err := b.selectCommon("", "1", nil, "").Limit(0, 1).GetRows()
+	defer rows.Close()
 	if err != nil {
 		return false, err
 	}
 
-	if obj.C.Int64 == 1 {
+	if rows.Next() {
 		return true, nil
 	} else {
 		return false, nil
