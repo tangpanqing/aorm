6e6d4e94971af7d84ac69a17b5e92276862d60c8
tangpanqing <tangpanqing@qq.com>
2023-03-07 09:19:03 +0800

    and func for get field comment

--- builder/cache.go
+++ builder/cache.go
@@ -7,6 +7,7 @@ import (
 type FieldInfo struct {
 	TablePointer uintptr
 	Name         string
+	TagMap       map[string]string
 }
 
 var TableMap = make(map[uintptr]string)
@@ -26,11 +27,25 @@ func Store(destList ...interface{}) {
 		for j := 0; j < valueOf.Elem().NumField(); j++ {
 			fieldPointer := valueOf.Elem().Field(j).Addr().Pointer()
 			key, _ := getFieldNameByStructField(typeof.Elem().Field(j))
+			tag := typeof.Elem().Field(j).Tag.Get("aorm")
+
 			setFieldMap(fieldPointer, FieldInfo{
 				TablePointer: tablePointer,
 				Name:         key,
+				TagMap:       getTagMap(tag),
 			})
 		}
+	}
+}
+
+func Comment(field interface{}) string {
+	fieldPointer := reflect.ValueOf(field).Pointer()
+	tagMap := getFieldMap(fieldPointer).TagMap
+	val, ok := tagMap["comment"]
+	if ok {
+		return val
+	} else {
+		return ""
 	}
 }
 
